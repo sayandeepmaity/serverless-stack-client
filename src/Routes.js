08e10c12aec6ce789fbd4f84aa8e06f258a8df71
Sayandeepmaity <sayandeepmaity777@gmail.com>
2024-04-19 23:26:44 +0530

    Setting up our React app

--- src/Routes.js
+++ src/Routes.js
@@ -2,11 +2,13 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./containers/Home";
 import NotFound from "./containers/NotFound"; // Update import path
+import Login from "./containers/Login"; // This line already includes the import for Login
 
 export default function MyRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route exact path="/" element={<Home />} />
+      <Route exact path="/login" element={<Login />} />
       {/* Finally, catch all unmatched routes */}
       <Route path="*" element={<NotFound />} />
     </Routes>
